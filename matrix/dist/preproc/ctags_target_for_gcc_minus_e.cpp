2c98561c0b69eec2e26fbceae55313844a42dfaf
JL <jo.kil.plays@gmail.com>
2021-11-30 16:19:03 +0100

    stuff

--- matrix/dist/preproc/ctags_target_for_gcc_minus_e.cpp
+++ matrix/dist/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,17 +1,35 @@
 # 1 "c:\\GIT\\ESP32-Projects\\matrix\\matrix.ino"
 # 2 "c:\\GIT\\ESP32-Projects\\matrix\\matrix.ino" 2
+# 3 "c:\\GIT\\ESP32-Projects\\matrix\\matrix.ino" 2
 
-PxMATRIX display(64,64,GPIO_NUM_22,GPIO_NUM_16, GPIO_NUM_19,GPIO_NUM_23,GPIO_NUM_18, GPIO_NUM_5, GPIO_NUM_15);
 
-void setup() {
-  display.setPanelsWidth(1);
-  display.setMuxPattern(BINARY);
-  display.begin(32);
-}
 
-// the loop function runs over and over again forever
-void loop() {
-  //display.fillScreen(display.color565(rand() % 255,rand() % 255,rand() % 255));display.fillScreen(1);
-  display.displayTestPattern(1);
-  //display.fillScreen(display.color565());
+
+
+// Change these to whatever suits
+#define R1_PIN GPIO_NUM_13
+#define G1_PIN GPIO_NUM_27
+#define B1_PIN GPIO_NUM_26
+#define R2_PIN GPIO_NUM_25
+#define G2_PIN GPIO_NUM_33
+#define B2_PIN GPIO_NUM_32
+#define A_PIN GPIO_NUM_19
+#define B_PIN GPIO_NUM_23
+#define C_PIN GPIO_NUM_18
+#define D_PIN GPIO_NUM_5
+#define E_PIN GPIO_NUM_15 /* required for 1/32 scan panels, like 64x64. Any available pin would do, i.e. IO32*/
+#define LAT_PIN GPIO_NUM_22
+#define OE_PIN GPIO_NUM_16
+#define CLK_PIN GPIO_NUM_14
+
+HUB75_I2S_CFG::i2s_pins _pins={GPIO_NUM_13, GPIO_NUM_27, GPIO_NUM_26, GPIO_NUM_25, GPIO_NUM_33, GPIO_NUM_32, GPIO_NUM_19, GPIO_NUM_23, GPIO_NUM_18, GPIO_NUM_5, GPIO_NUM_15 /* required for 1/32 scan panels, like 64x64. Any available pin would do, i.e. IO32*/, GPIO_NUM_22, GPIO_NUM_16, GPIO_NUM_14};
+HUB75_I2S_CFG mxconfig(
+ 64, // Module width
+ 64, // Module height
+ 1, // chain length
+ _pins, // pin mapping
+);
+
+void setup () {
+  MatrixPanel_I2S_DMA dma_display = new MatrixPanel_I2S_DMA(mxconfig);
 }
